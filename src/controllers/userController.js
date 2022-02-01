eaf292566193c6064886d4baa2d5a67188776501
SatvikGupta <satvik.sli@gmail.com>
2022-02-02 03:11:04 +0530

    Add image upload, delete and get

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,45 @@
 const User = require("../models/user");
 
+async function getUserImage(req, res) {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user || !user.avatar) {
+      return res.status(404).send();
+    }
+    res.set("Content-Type", "image/png");
+    res.send(user.avatar);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+async function uploadImage(req, res) {
+  try {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+}
+
+async function deleteImage(req, res) {
+  try {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    req.status(500).send(error.message);
+  }
+}
+async function uploadImageErrorHandler(err, req, res, next) {
+  if (err.code === "LIMIT_FILE_SIZE") {
+    res.status(400).send({ error: "File too big" });
+  } else {
+    res.status(400).send({ error: "Invalid file" });
+  }
+}
+
 async function addUser(req, res) {
   const user = new User(req.body);
   try {
@@ -79,4 +119,8 @@ module.exports = {
   loginUser,
   logoutUser,
   logoutAllUsers,
+  uploadImage,
+  uploadImageErrorHandler,
+  deleteImage,
+  getUserImage,
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,20 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const auth = require("../middleware/auth");
+const multer = require("multer");
 
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|png|jpeg)$/)) {
+      return cb(new Error("File must be jpg,png or jpeg."));
+    }
+    cb(undefined, true);
+  },
+});
+router.get("/:id/avatar", userController.getUserImage);
 router.get("/me", auth, (req, res) => {
   res.send(req.user);
 });
@@ -11,6 +24,14 @@ router.post("/logoutAll", auth, userController.logoutAllUsers);
 router.post("/login", userController.loginUser);
 router.post("/", userController.addUser);
 router.patch("/me", auth, userController.updateUser);
+router.delete("/me/avatar", auth, userController.deleteImage);
 router.delete("/me", auth, userController.deleteUser);
+router.post(
+  "/me/avatar",
+  auth,
+  upload.single("upload"),
+  userController.uploadImage,
+  userController.uploadImageErrorHandler
+);
 
 module.exports = router;
